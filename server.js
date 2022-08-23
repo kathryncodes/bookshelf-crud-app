2a3e8c7e6101dc01f1cb0b8e34cc8afe674641bf
kathryncodes <katnicjohn@gmail.com>
2022-08-23 16:29:20 -0700

    fixv7

--- server.js
+++ server.js
@@ -4,16 +4,6 @@ const MongoClient = require('mongodb').MongoClient;
 const PORT = process.env.PORT || 8000;
 require('dotenv').config();
 
-//Mongo Connection fix attempt
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = process.env.DB_STRING;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 
 let db, 
     dbConnectionStr = process.env.DB_STRING, 
